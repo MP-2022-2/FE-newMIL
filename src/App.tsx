a1c55858f8dc2cf24e783d07734123077982f399
Jae Han Song <rfv1479@gmail.com>
2022-09-01 16:57:41 +0900

    Chore : FE 개발 환경 세팅
    Eslint, prettier 세팅
    컴포넌트 폴더 분류

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Main from './Pages/Main';
+
+const App = () => (
+  <>
+    미프 하이
+    <Main />
+    <Main />
+    <h1>ㅎㅇㅎㅇㅎ</h1>
+  </>
+);
+
+export default App;
---
+++ src/Pages/Main/index.tsx
@@ -0,0 +1,9 @@
+import { MainContainer } from './style';
+
+const main = () => (
+  <>
+    <MainContainer>하이하이</MainContainer>
+  </>
+);
+
+export default main;
