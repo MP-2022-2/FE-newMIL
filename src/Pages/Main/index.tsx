6678666f05ec918c6b3a65b593e96991269600c0
Jae Han Song <rfv1479@gmail.com>
2022-12-09 21:32:20 +0900

    Feat #24 : 메인 페이지 레이아웃

--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -1,12 +1,71 @@
 import Header from '@/Components/Header';
-import { MainContainer } from './style';
+import useAnimation from '@/Utils/Hooks/useAnimation';
+import useScrollFade from '@/Utils/Hooks/useScrollFade';
+import talkComputer from '@/assets/animation/talk-computer.json';
+import GridSection from '@/Components/Section';
+import {
+  MainContainer,
+  BannerText,
+  BannerContainer,
+  BannerAnimation,
+  BannerBackground,
+  SubMenuWrapper,
+  SubMenuContainer,
+  NoticeContainer,
+  NoticeWrapper,
+  SubMenu,
+  Notice,
+} from './style';
 
-const MainPage = () => (
-  <>
-    <Header />
-    <MainContainer>
-      우리 과만의 커넥팅 라이브러리 <br /> 언더바
-    </MainContainer>
-  </>
-);
-export default MainPage;
+export default function MainPage() {
+  const animationStudy = useAnimation(talkComputer, true, 20, 20);
+  const FadeIn = useScrollFade();
+  const FadeIn2 = useScrollFade('up', 1.5, 1);
+
+  return (
+    <>
+      <Header />
+      <MainContainer>
+        <BannerContainer>
+          <BannerBackground />
+          <BannerAnimation {...animationStudy} />
+          <BannerText>
+            <h3 {...FadeIn}>우리 과만의 커넥팅 라이브러리</h3>
+            <h1 {...FadeIn2}>언더바</h1>
+          </BannerText>
+        </BannerContainer>
+        <SubMenuWrapper>
+          <h2>우리들만의 발자취를 따라가 봐요</h2>
+          <SubMenuContainer>
+            <GridSection col4>
+              <SubMenu>최신글</SubMenu>
+            </GridSection>
+            <GridSection col4>
+              <SubMenu>인기글</SubMenu>
+            </GridSection>
+            <GridSection col4>
+              <SubMenu>수강 현황</SubMenu>
+            </GridSection>
+          </SubMenuContainer>
+        </SubMenuWrapper>
+        <NoticeWrapper>
+          <h2>공지사항</h2>
+          <NoticeContainer>
+            <GridSection col3>
+              <Notice>공지사항</Notice>
+            </GridSection>
+            <GridSection col3>
+              <Notice>공지사항</Notice>
+            </GridSection>
+            <GridSection col3>
+              <Notice>공지사항</Notice>
+            </GridSection>
+            <GridSection col3>
+              <Notice>공지사항</Notice>
+            </GridSection>
+          </NoticeContainer>
+        </NoticeWrapper>
+      </MainContainer>
+    </>
+  );
+}
--- src/Pages/Main/style.ts
+++ src/Pages/Main/style.ts
@@ -5,3 +5,89 @@ export const MainContainer = styled.section`
   ${theme.common.flexCenterColumn};
   text-align: center;
 `;
+
+export const BannerText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+
+  color: white;
+`;
+
+export const BannerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 35rem;
+  width: 100%;
+  background-color: ${theme.color.ajouBlue};
+`;
+
+export const BannerBackground = styled.div`
+  position: absolute;
+  opacity: 0.25;
+  background-image: url('/pictures/main/landscape.png');
+  height: 35rem;
+  width: 100%;
+`;
+
+export const BannerAnimation = styled.div`
+  margin: 0 auto;
+`;
+
+export const SubMenuWrapper = styled.section`
+  display: block;
+  margin: 0 auto;
+  width: 100%;
+  max-width: ${theme.layout.maxWidth};
+
+  h2 {
+    padding: 5rem 0 2rem;
+  }
+`;
+
+export const SubMenuContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  max-width: ${theme.layout.maxWidth};
+`;
+
+export const SubMenu = styled.div`
+  display: block;
+  height: 12rem;
+  width: 100%;
+
+  border-radius: 0.625rem;
+  border: 1px solid ${theme.color.ajouSky};
+  ${theme.common.shadowDefault};
+  ${theme.common.pushAnimation};
+`;
+
+export const NoticeContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  max-width: ${theme.layout.maxWidth};
+
+  margin: 3rem 0;
+`;
+
+export const Notice = styled.div`
+  width: 100%;
+  height: 10rem;
+  border-radius: 0.625rem;
+  border: 1px solid ${theme.color.ajouSky};
+  ${theme.common.shadowDefault};
+  ${theme.common.pushAnimation};
+`;
+
+export const NoticeWrapper = styled(SubMenuWrapper)`
+  h2 {
+    padding: 5rem 0 0 0;
+  }
+  margin-bottom: 10rem;
+`;
